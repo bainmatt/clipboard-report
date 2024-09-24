250fda7fb43d337a87fc5c7732a958a93dbfd478
bainmatt <52426789+bainmatt@users.noreply.github.com>
2024-09-24 15:04:50 -0400

    DOC: Add support for different file types and improve page navigation

--- src/analysis.py
+++ src/analysis.py
@@ -4,8 +4,9 @@ Utility functions for data wrangling, preprocessing, and analysis.
 
 import pandas as pd
 
-# from src.workflow import doctest_function
+from src.inspection import display
 
 # z: str = 4
 
 x = pd.DataFrame([1, 2, 3])
+display("x", globs=globals())
